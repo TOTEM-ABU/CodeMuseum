50aa3c56f005e9b077740009c2dab58a56f035d2
Muhammad Yusuf <yuvsufn@gmail.com>
2025-08-03 22:52:58 +0500

    Update auth.service.ts

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -51,7 +51,7 @@ export class AuthService {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: 'none',
       maxAge: 1000 * 60 * 60 * 24,
     });
@@ -85,7 +85,7 @@ export class AuthService {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: 'none',
       maxAge: 1000 * 60 * 60 * 24,
     });
