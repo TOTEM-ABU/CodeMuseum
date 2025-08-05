6ac1d85de8e278b4102c67da75d8e4ec95960e60
Muhammad Yusuf <yuvsufn@gmail.com>
2025-08-05 17:23:47 +0500

    newupdate

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -240,6 +240,7 @@ export class UserService {
           PostCategory: {
             select: {
               id: true,
+              category: true
             },
           },
           comments: {
